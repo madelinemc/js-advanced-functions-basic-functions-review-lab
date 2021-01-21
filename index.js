efef9836af8744791e12f0b56f1351bc8a60dfde
madelinemc <madeline.coons@gmail.com>
2021-01-21 16:19:49 -0600

    commit tree e570c507680c0847227b03ee6ccdc608ada112fb

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(adj="*") {
+    return function(adjective="special") {
+        return `You are ${adj}${adjective}${adj}!`
+    }
+}
+
+const Calculator = {
+    add: function(a, b) {return a + b},
+    subtract: function(a, b) {return a - b},
+    multiply: function(a, b) {return a * b},
+    divide: function(a, b) {return a / b}
+}
+
+function actionApplyer(integer, array) {
+    let start = integer
+    for (let i = 0; i < array.length; i++){
+        start = array[i](start)
+    }
+    return start
+}
